352852d5f6d5d07ae0d2b8e13c2b7353ed097a03
karan <karan8872@gmail.com>
2024-12-06 19:30:45 +0530

    update config.js

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,2 +1,3 @@
-export const Server_URL  = 'http://localhost:5000/'
+// export const Server_URL  = 'http://localhost:5000/'
 export const Server_URL2  = 'http://localhost:5000'
+export const Server_URL = import.meta.env.VITE_BACKEND_URL;
