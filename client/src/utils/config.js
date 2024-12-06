f76a5b46729fb50fd33171b9f010ab40784a5561
karan <karan8872@gmail.com>
2024-12-07 01:50:35 +0530

    update home.jsx

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,3 +1,4 @@
 // export const Server_URL  = 'http://localhost:5000/'
 export const Server_URL2  = 'http://localhost:5000'
+console.log("VITE_BACKEND_URL"+ VITE_BACKEND_URL)
 export const Server_URL = import.meta.env.VITE_BACKEND_URL;
