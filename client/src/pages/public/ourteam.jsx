e0042f1806e4d27a1d2668394779b89dd4065819
karan <karan8872@gmail.com>
2024-12-07 12:24:41 +0530

    update ourteam

--- client/src/pages/public/ourteam.jsx
+++ client/src/pages/public/ourteam.jsx
@@ -49,7 +49,7 @@ export default function OurTeam(){
                     <div className="single-team-card" datacue="fadeIn" dataduration="1000">
                     <div className="team-img" style={{ height: '200px', overflow: 'hidden' }}>
                             
-                            <img src={x.photo? (`${Server_URL2}${x.photo}`) : '/photo1.png'} alt="category"
+                            <img src={x.photo? x.photo : '/photo1.png'} alt="category"
                 style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
                 />
                         </div>
